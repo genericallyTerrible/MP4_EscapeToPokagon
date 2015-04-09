a8d6c101b42f37c10c9ea66ebb86583028ae0f32
mtbjj <m-babej@onu.edu>
2015-04-09 18:30:54 -0400

    Created class structure
    and created frame

---
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Inventory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mp4_escape_to_pokagon_package;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mtbjj
+ */
+public class Inventory {
+    ArrayList<Pickup> items;
+    
+    Inventory() {
+        items = new ArrayList();
+    }
+}
---
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Person.java
@@ -0,0 +1,23 @@
+package mp4_escape_to_pokagon_package;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mtbjj
+ */
+public class Person {
+    Inventory inventory;
+    int numPickups;
+    
+    
+    Person() {
+        inventory = new Inventory();
+    }
+    
+    
+}
