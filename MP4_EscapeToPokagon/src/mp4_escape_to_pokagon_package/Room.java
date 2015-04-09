f920e3c96df75516a5116d36bea23d68788c0ab2
Andrew <Andrew@140.228.123.236>
2015-04-09 19:57:59 -0400

    Room class which should successfully store all data from a properly set up txt file.  Hasn't been tested yet.

--- MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Room.java
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Room.java
@@ -6,10 +6,40 @@
 
 package mp4_escape_to_pokagon_package;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author mtbjj
  */
 public class Room {
+    int northRoom;
+    int eastRoom;
+    int southRoom;
+    int westRoom;
+    int pointValue;
+    String shortDescription;
+    String longDescription;
+    Inventory inventory;
     
+    Room(String fileName) throws FileNotFoundException, IOException
+    {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        northRoom = Integer.parseInt(reader.readLine());
+        eastRoom = Integer.parseInt(reader.readLine());
+        southRoom = Integer.parseInt(reader.readLine());
+        westRoom = Integer.parseInt(reader.readLine());
+        pointValue = Integer.parseInt(reader.readLine());
+        shortDescription = reader.readLine();
+        String check = reader.readLine();
+        while(!check.equals("END"))
+        {
+            longDescription += check + "\n";
+            check = reader.readLine();
+        }
+        longDescription = longDescription.substring(0, longDescription.length()-2);
+    }
 }
