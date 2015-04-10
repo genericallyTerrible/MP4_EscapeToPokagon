a000d5faaa0c52f8cef000a656e58fc9cc716bd0
mtbjj <m-babej@onu.edu>
2015-04-10 07:40:43 -0400

    Added some comments to compassLabel

--- MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/mp4Frame.java
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/mp4Frame.java
@@ -342,10 +342,8 @@ public class mp4Frame extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new mp4Frame().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new mp4Frame().setVisible(true);
         });
     }
 
