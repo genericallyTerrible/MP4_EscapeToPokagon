c64920f85fe32e1f0770427468b9c5cca0e7dcb5
genericallyTerrible <jmoehrnkel@gmail.com>
2015-04-10 05:49:40 -0400

    edited content access and behavior etc

--- MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Map.java
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Map.java
@@ -112,7 +112,7 @@ public class Map {
     }
     
     public String look(){
-        return currentRoom.getLongDescription();
+        return currentRoom.getLongDescription() + currentContents();
     }
     
     public String currentContents() {
