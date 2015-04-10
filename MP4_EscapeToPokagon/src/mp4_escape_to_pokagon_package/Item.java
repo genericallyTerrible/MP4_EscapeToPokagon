024de9e3bf95148a2a37fdfa4b4571366dc6efc7
genericallyTerrible <jmoehrnkel@gmail.com>
2015-04-09 22:00:46 -0400

    Added all 15 Compass Possibilities and Item etc

--- MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Item.java
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Item.java
@@ -11,9 +11,32 @@ package mp4_escape_to_pokagon_package;
  * @author mtbjj
  */
 public class Item {
-    int location;
     
-    Item() {
-        
+    private String name;
+    private int pointValue;
+    private int scoreRoom;
+    private int startLocation;
+    
+    Item(String name, int pointValue, int scoreRoom, int startLocation){
+        this.name          = name;
+        this.pointValue    = pointValue;
+        this.scoreRoom     = scoreRoom;
+        this.startLocation = startLocation;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int scoreRoom() {
+        return scoreRoom;
+    }
+    
+    public int pointValue() {
+        return pointValue;
+    }
+    
+    public int startLocation() {
+        return startLocation;
     }
 }
