cd95fd7d6edf14c0dbbe4243693c62e589d4d2c6
Andrew <Andrew@140.228.123.236>
2015-04-09 23:03:00 -0400

    small Map changes

--- MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Map.java
+++ MP4_EscapeToPokagon/src/mp4_escape_to_pokagon_package/Map.java
@@ -5,10 +5,29 @@
  */
 package mp4_escape_to_pokagon_package;
 
+import java.io.IOException;
+
 /**
  *
  * @author Andrew
  */
 public class Map {
+    Room[] rooms;
+    Room currentRoom;
+    private final int NUM_ROOMS = 5;
     
+    Map() throws IOException
+    {
+        rooms = new Room[NUM_ROOMS];
+        for(int i = 0; i < NUM_ROOMS; i++)
+        {
+            rooms[i] = new Room("src/roomspackage/room" + i + ".txt");
+        }
+        currentRoom = rooms[0];
+    }
+    
+    public boolean[] getMoveableDirections()
+    {
+        
+    }
 }
